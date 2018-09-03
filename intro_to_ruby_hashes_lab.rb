72f467397f031cd579629b4f1758c42a2c4c0bfb
Amalie S <amaliesmidth@gmail.com>
2018-09-03 14:34:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,7 +47,9 @@ def monopoly_with_fourth_tier
         {
           "mortgage_value" => "$100"
         }, 
-        :pennsylvania_railroad => {},
+        :pennsylvania_railroad => {
+          
+        },
         :b_and_o_railroad => {},
         :shortline => {}
       }, 
